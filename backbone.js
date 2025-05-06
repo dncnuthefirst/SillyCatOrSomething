9c471ceebc66be5c47f05caa88456907a5cab14e
dncnuthafirst <148245840+dncnuthefirst@users.noreply.github.com>
2025-05-06 18:39:33 +0800

    Add files via upload

---
+++ backbone.js
@@ -0,0 +1,36 @@
+const CatPicture = document.getElementsByClassName("CatPicture")
+const MeowCounter = document.getElementsByClassName("MeowCounter")
+
+var Meows = 0
+
+function RandomDec(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function UpdateDocument(){
+    MeowCounter[0].textContent = "Meows: " + Meows;
+
+    // Instant scale-up
+    CatPicture[0].style.transition = "none";
+    CatPicture[0].style.transform = "scale(1.1)";
+
+    // Shrink after a while
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            CatPicture[0].style.transition = "transform .5s ease-in-out";
+            CatPicture[0].style.transform = "scale(1)";
+        });
+    });
+}
+
+// Plays some sound
+function PlayRandomSound() {
+    const Int = Math.round(RandomDec(1,8));
+    let sound = new Audio("Sounds/MeowNormal" + Int + ".mp3");
+    sound.playbackRate = RandomDec(0.8,1);
+    sound.play();
+    Meows++;
+    UpdateDocument()
+}
+
+UpdateDocument()
